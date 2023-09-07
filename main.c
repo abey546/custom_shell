97f591e1108a34cf632af9251b3e9a4e76a682f3
abey546 <abeyhassan546>
2023-09-07 06:36:01 -0700

    custom_shell

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on succesful program execution.
+ */
+int main(void)
+{
+	handle_input();
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* Function prototypes*/
+void trim_newline(char **str);
+size_t _strlen(const char *str);
+int count_arguments(const char *str);
+int _strcmp(const char *s1, const char *s2);
+char **tokenize_input(const char *input);
+void execute_command(char **args);
+void handle_input(void);
+
+#endif /* _SHELL_H_ */
+
